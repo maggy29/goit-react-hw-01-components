7851c8ded320c08a2114ef417c913133b3b6aca4
Masha Grom <grom.m.yu@gmail.com>
2020-06-06 14:48:36 +0300

    Updates

---
+++ precache-manifest.9407c88cead791deb3fef824d913b6e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48c996ed79c810c86979f5e48cff4b1",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "dc22f97504245a959c3c",
+    "url": "/goit-react-hw-01-components/static/css/main.ee5d037e.chunk.css"
+  },
+  {
+    "revision": "390183fa2a980628dba6",
+    "url": "/goit-react-hw-01-components/static/js/2.cd324757.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.cd324757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc22f97504245a959c3c",
+    "url": "/goit-react-hw-01-components/static/js/main.20b5115d.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
